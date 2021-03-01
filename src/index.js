0848d9168b22abcb58c2a566650497e245b35920
Green1ce <cha1iiep@mail.ru>
2021-03-01 21:58:02 +0300

    feat: completed functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length < 1) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (min > array[i]) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length < 1){
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length < 1) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      avg += array[i];
+    }
+    return avg/array.length;
+  }
 }
